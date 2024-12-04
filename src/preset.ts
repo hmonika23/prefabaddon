561f0d786a280026c6f8bd8d0658201dbfba8902
hmonika23 <monika.hamand@wavemaker.com>
2024-12-04 08:35:02 +0530

    Modifies preset.js file to create a json file

--- src/preset.ts
+++ src/preset.ts
@@ -1,14 +1,63 @@
-// You can use presets to augment the Storybook configuration
-// You rarely want to do this in addons,
-// so often you want to delete this file and remove the reference to it in package.json#exports and package.json#bunder.nodeEntries
-// Read more about presets at https://storybook.js.org/docs/addons/writing-presets
+import * as fs from 'fs';
+import * as path from 'path';
+import glob from 'glob';
+
+// Function to generate the JSON file
+const generateJsonFile = () => {
+  try {
+    const storybookRoot = process.cwd(); // Root directory of the Storybook project
+
+    console.log("storybookRoot",storybookRoot);
+    const storiesGlob = path.join(storybookRoot, 'src/**/*.stories.@(js|jsx|ts|tsx)');
+
+    console.log("storiesGlob", storiesGlob);
+    const outputFilePath = path.join(storybookRoot, 'wmprefab-config.json');
+      console.log("outputFilePath", outputFilePath);
+
+    const components: Array<any> = [];
+
+    // Find all story files
+    const storyFiles = glob.sync(storiesGlob);
+    console.log(`Found ${storyFiles.length} story files.`);
+
+    for (const file of storyFiles) {
+      const componentName = path.basename(file, path.extname(file)).toLowerCase();
+      components.push({
+        name: componentName,
+        version: '1.0.0',
+        displayName: componentName,
+        baseDir: './components',
+        module: `require('./${componentName}/${componentName}').default`,
+        include: [`./${componentName}/${componentName}.js`],
+        props: [
+          {
+            name: 'exampleProp',
+            type: 'string',
+            defaultValue: 'exampleValue',
+          },
+        ],
+        packages: [],
+      });
+    }
+
+    // Write to JSON file
+    const jsonData = { components };
+    console.log("jsonData", jsonData);
+    fs.writeFileSync(outputFilePath, JSON.stringify(jsonData, null, 2));
+    console.log(`Generated JSON file at: ${outputFilePath}`);
+  } catch (error) {
+    console.error('Error generating wmprefab-config.json:', error);
+  }
+};
 
 export const viteFinal = async (config: any) => {
-  console.log("This addon is augmenting the Vite config");
+  console.log('This addon is augmenting the Vite config');
+  generateJsonFile(); // Generate the JSON file before returning the Vite config
   return config;
 };
 
-export const webpack = async (config: any) => {
-  console.log("This addon is augmenting the Webpack config");
+export const webpackFinal = async (config: any) => {
+  console.log('This addon is augmenting the Webpack config');
+  generateJsonFile(); // Generate the JSON file before returning the Webpack config
   return config;
 };
